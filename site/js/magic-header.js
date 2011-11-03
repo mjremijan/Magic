ed20ad3806601b988ea2ed91d3bbc865e75e2a61
Michael Remijan <mjremijan@yahoo.com>
2011-11-03 16:06:26 -0500

    cleanup css name, move header and menu to a javascript file, start on philosophy.

---
+++ site/js/magic-header.js
@@ -0,0 +1,43 @@
+function writeHeader(active) {
+	document.write(getHeader(active));
+}
+function getHeader(active)
+{
+	var menu = "";
+	
+	menu += " <div id=\"header-wrapper\"> ";
+	menu += "	<div id=\"header-logo\"> ";
+	menu += "		<img alt=\"magic building block logo\" src=\"images/header/logo.png\" /> ";
+	menu += "	</div> ";
+	menu += "	<div id=\"header-menu\"> ";
+	menu += "		<div id=\"header-menu-links\"> ";
+	menu += "			"+getHome(active)+" | "+getContactUs(active)+" ";
+	menu += "		</div> ";
+	menu += "		<div id=\"header-menu-slogan\"> ";
+	menu += "			<img src=\"images/header/serving-30-years.png\" alt=\"serving over 30 years\" /> ";
+	menu += "		</div> ";
+	menu += "	</div><!--header-logo--> ";
+	menu += " </div><!--header-wrapper--> ";
+	
+	return menu;
+}
+function getContactUs(active)
+{
+	var clas = "";
+	var page = "contactus.html";
+	if (active === "contactus") {
+		clas = "class=\"active\"";
+		page = "#";
+	}
+	return "<a href=\""+page+"\" "+clas+">contact us</a>";
+}
+function getHome(active)
+{
+	var clas = "";
+	var page = "index.html";
+	if (active === "home") {
+		clas = "class=\"active\"";
+		page = "#";
+	}
+	return "<a href=\""+page+"\" "+clas+">home</a>";	
+}
---
+++ site/js/magic-menu.js
@@ -0,0 +1,63 @@
+function writeMenu(active) {
+	document.write(getMenu(active));
+}
+function getMenu(active)
+{
+	var menu = "";
+	
+	menu += " <div id=\"menu-wrapper\"> ";
+	menu += " 	<div id=\"menu-centered-dropshadow\"> ";
+	menu += " 		<img src=\"images/menu/centered-dropshadow.png\"/> ";
+	menu += " 	</div> ";
+	menu += " 	<div id=\"menu-centered-items\"> ";
+	menu += " 	   <ul> ";	
+	menu += " 		  " + getLocation(active);
+	menu += " 		  " + getPhilosophy(active);
+	menu += " 		  " + getRates(active);
+	menu += " 		  " + getCurriculum(active);
+	menu += " 	   </ul> ";
+	menu += " 	</div><!--menu-centered-items--> ";
+	menu += " </div><!--menu-wrapper--> ";
+	
+	return menu;
+}
+function getCurriculum(active)
+{
+	var clas = "";
+	var page = "curriculum.html";
+	if (active === "curriculum") {
+		clas = "class=\"active\"";
+		page = "#";
+	}
+	return "<li class=\"first\"><a "+clas+" href=\""+page+"\">Curriculum</a></li> ";
+}
+function getRates(active)
+{
+	var clas = "";
+	var page = "rates.html";
+	if (active === "rates") {
+		clas = "class=\"active\"";
+		page = "#";
+	}
+	return "<li class=\"first\"><a "+clas+" href=\""+page+"\">Rates</a></li> ";
+}
+function getPhilosophy(active)
+{
+	var clas = "";
+	var page = "philosophy.html";
+	if (active === "philosophy") {
+		clas = "class=\"active\"";
+		page = "#";
+	}
+	return "<li class=\"first\"><a "+clas+" href=\""+page+"\">Philosophy</a></li> ";
+}
+function getLocation(active)
+{
+	var clas = "";
+	var page = "locations.html";
+	if (active === "locations") {
+		clas = "class=\"active\"";
+		page = "#";
+	}
+	return "<li class=\"first\"><a "+clas+" href=\""+page+"\">Locations</a></li> ";
+}
