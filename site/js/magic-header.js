5c4c7f612ab088fed3ba8a850abc09c451373878
Michael Remijan <mjremijan@yahoo.com>
2016-05-16 22:15:17 -0500

    Added Facebook "like" button and added "visit us on facebook" images to index.html and contact.html

--- site/js/magic-header.js
+++ site/js/magic-header.js
@@ -1,3 +1,16 @@
+
+
+document.write("<div id=\"fb-root\"></div>");
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+
 function writeHeader(active) {
 	document.write(getHeader(active));
 }
@@ -11,7 +24,10 @@ function getHeader(active)
 	menu += "	</div> ";
 	menu += "	<div id=\"header-menu\"> ";
 	menu += "		<div id=\"header-menu-links\"> ";
-	menu += "			"+getHome(active)+" | "+getContactUs(active)+" ";
+	menu += "			"+getHome(active)+" | "+getContactUs(active)+"&nbsp;&nbsp;";
+    
+    menu += "<div class=\"fb-like\" data-href=\"https://www.facebook.com/Magic-Building-Block-Preschool-Childcare-Center-133052263412902/\" data-width=\"200\" data-layout=\"button\" data-action=\"like\" data-show-faces=\"false\" data-share=\"false\"></div>";
+    
 	menu += "		</div> ";
 	menu += "		<div id=\"header-menu-slogan\"> ";
 	menu += "			<img src=\"images/header/serving-30-years.png\" alt=\"serving over 30 years\" /> ";
